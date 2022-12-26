96863dd12ba105e8ce6052770d81115a8c606a16
Aitor <aitorllj93@gmail.com>
2022-12-26 15:26:47 +0100

    minor changes

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,19 +6,19 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Behave-Graph',
+  title: 'Behave-Graph/Struct',
   tagline:
-    '"behavior graphs" as a portable TypeScript library with no required external run-time dependencies',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+    'Behave-Graph Profile for manipulating structured data (Objects & Lists)',
+  url: 'https://aitorllamas.com',
+  baseUrl: '/behave-graph-struct',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'bhouston', // Usually your GitHub org/user name.
-  projectName: 'behave-graph', // Usually your repo name.
+  organizationName: 'aitorllj93', // Usually your GitHub org/user name.
+  projectName: 'behave-graph-struct', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,13 +37,15 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/bhouston/behave-graph/tree/main/website/'
+          editUrl:
+            'https://github.com/aitorllj93/behave-graph-struct/tree/main/website/'
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/bhouston/behave-graph/tree/main/website/'
+          editUrl:
+            'https://github.com/aitorllj93/behave-graph-struct/tree/main/website/'
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
@@ -71,7 +73,7 @@ const config = {
           },
           { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://github.com/bhouston/behave-graph',
+            href: 'https://github.com/aitorllj93/behave-graph-struct',
             label: 'GitHub',
             position: 'right'
           }
@@ -111,7 +113,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/bhouston/behave-graph'
+                href: 'https://github.com/aitorllj93/behave-graph-struct'
               }
             ]
           }
